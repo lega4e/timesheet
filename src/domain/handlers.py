3e427d81d0e2780d7ee7bfe8cbc819e31a52aca2
nvxden <clever411@yandex.ru>
2023-03-16 15:05:44 +0300

    hothotfix\

--- src/domain/handlers.py
+++ src/domain/handlers.py
@@ -11,7 +11,7 @@ users = locator.userRepo()
 
 def user_finder(func):
   def wrapper(m, res=False):
-    func(users.find(m.chat.id), m, res)
+    func(users.find(m.chat.id) if m.chat.id > 0 else None, m, res)
   return wrapper
 
 
